4112ee4de2a37be149befa78b16cb23e32245325
Kendell R <KTibow@users.noreply.github.com>
2021-06-02 13:04:58 -0700

    remove commented code

--- js/contentScripts/i-ready.com.js
+++ js/contentScripts/i-ready.com.js
@@ -1,24 +1,4 @@
-/*var observer = new MutationObserver(() => {
-  var vidWrap = document.querySelector("#tutorialPlayerWrapper");
-  console.log(vidWrap);
-  if (vidWrap && !vidWrap.querySelector(".skipVid")) {
-    var skipBtn = document.createElement("button");
-    skipBtn.innerHTML = "⏩";
-    skipBtn.onclick = () => {
-      var vid = document.querySelector("video");
-      vid.currentTime = vid.duration;
-    };
-    skipBtn.style.position = "absolute";
-    skipBtn.style.border = "none";
-    skipBtn.style.bottom = "4pt";
-    skipBtn.style.left = "calc(48px + 8pt)";
-    skipBtn.style.height = "36px";
-    skipBtn.className = "skipVid";
-    vidWrap.appendChild(skipBtn);
-  }
-});*/
 setInterval(() => {
-  //observer.observe(body, {childList: true, subtree: true});
   var vidWrap = document.querySelector("#tutorialPlayerWrapper");
   console.log(vidWrap);
   if (vidWrap && !vidWrap.querySelector(".skipVid")) {
